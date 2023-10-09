2a1f45f82b725550fd0024f02ba6b66cdd700a2c
pankajkoirala <pankaj.developer.77@gmail.com>
2023-10-09 09:08:37 +0545

    initial commit

---
+++ constant/data.ts
@@ -0,0 +1,58 @@
+export const data = [
+  {
+    id: "DATA-FETCHING",
+    title: "Data Fetching",
+    header: "Fetch data from any sources",
+    lists: [
+      "Frequency scheduling for automated data fetchers",
+      "Pre-configured fetcher tools",
+    ],
+    image: "",
+  },
+  {
+    id: "DATA-EXTRACTION",
+
+    title: "Data Extraction",
+    header: "Modular Data Extraction Engine",
+    lists: [
+      "Supports any structured, semi-structured and unstructured digital source",
+    ],
+    image: "",
+  },
+  {
+    id: "DATA-TRANSFORMATION",
+
+    title: "Data Transformation",
+    header: "Transforms data to fit your product",
+    lists: [
+      "Intelligent data parsers to read/transform semi and unstructured data from source files",
+      "Data Cleanup within the dataset",
+      "Output structured data with valid metabdata",
+    ],
+    image: "",
+  },
+  {
+    id: "DATA-QUALITY",
+
+    title: "Data Quality",
+    header: "Ensure quality of output data",
+    lists: [
+      "Fix / Remove Duplicate, Incomplete, corrupted data",
+      "Reformat data",
+      "Perform validation both at the source and end of data pipeline",
+    ],
+    image: "",
+  },
+  {
+    id: "REPORTING",
+
+    title: "Reporting",
+    header: "Configurable reporting system",
+    lists: [
+      "Create Ad hoc reports",
+      "Reports for Management, Regulators and Auditors",
+      "Closure Reports and Ageing Analysis",
+    ],
+    image: "",
+  },
+]
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,63 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss"
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "gradients-grey":
+          "linear-gradient(133deg, rgba(218, 221, 226, 0.80) 0%, rgba(218, 221, 226, 0.60) 100%)",
+        "gradients-white":
+          " linear-gradient(142deg, rgba(255, 255, 255, 0.70) 0%, rgba(255, 255, 255, 0.40) 100%)",
+      },
+      screens: {
+        mobile: { min: "320px", max: "640px" },
+        tablet: { min: "640px", max: "768px" },
+      },
+      colors: {
+        background: "#F9F9F7",
+        background2: "#EDE9FB",
+        "primary-black": "#2B2B2B",
+        "secondary-black": "#4B4E4B",
+        "brand-primary": "#5F4BB6",
+      },
+
+      fontSize: {
+        h1: [
+          "48px",
+          {
+            fontWeight: "700",
+            lineHeight: "normal",
+          },
+        ],
+        h2: [
+          "32px",
+          {
+            fontWeight: "700",
+            lineHeight: "normal",
+          },
+        ],
+        medium: [
+          "20px",
+          {
+            fontWeight: "700",
+            lineHeight: "normal",
+          },
+        ],
+        normal: [
+          "16px",
+          {
+            fontWeight: "400",
+            lineHeight: "normal",
+          },
+        ],
       },
     },
   },
